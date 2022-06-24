917907cb0655d494e767cbf822f49412c58bd367
eia485 <eia485git@gmail.com>
2022-06-24 11:27:00 -0700

    Add project files.

---
+++ boolchatosc/Program.cs
@@ -0,0 +1,60 @@
+﻿using boolchatosc;
+using System.Net;
+Console.WriteLine("program started");
+
+//define constants and instances
+
+SimpleOSC.OSCMessage t = new SimpleOSC.OSCMessage { path = "/avatar/parameters/boolchat", arguments = new object[1] { 1 }, typeTag = "" };
+SimpleOSC.OSCMessage f = new SimpleOSC.OSCMessage { path = "/avatar/parameters/boolchat", arguments = new object[1] { 0 }, typeTag = "" };
+
+statemngr oscstate = new("osc");
+statemngr wsstate = new("WebSocket");
+
+Console.WriteLine("conecting to boolchat websocket");
+wsmngr ws = new(new("wss://logix.newweb.page/boolchat"));
+
+SimpleOSC OSC = new();
+byte[] oscBuf = new byte[65535];
+List<SimpleOSC.OSCMessage> msg = new List<SimpleOSC.OSCMessage>();
+
+Console.WriteLine("connecting to osc");
+
+OSC.OpenClient(9001);
+OSC.SetUnconnectedEndpoint(new IPEndPoint(IPAddress.Loopback, 9000));
+
+
+
+
+while (true)
+{
+    OSC.GetIncomingOSC(msg);
+    foreach (SimpleOSC.OSCMessage m in msg)
+    {
+        if (m.path == "/avatar/parameters/boolchat" && m.arguments.Length == 1 && m.arguments[0].GetType() == typeof(bool))
+            oscstate.cur = (bool)m.arguments[0];
+    }
+    msg.Clear();
+
+    bool? wsIn = ws.receive();
+    if (wsIn.HasValue)
+        wsstate.cur = wsIn.Value;
+
+    if (oscstate.cur != wsstate.cur)
+    {
+        Console.WriteLine("updating other");
+        if (oscstate.changed)
+        {
+            ws.send(oscstate.cur);
+            wsstate.cur = (oscstate.cur);
+        }
+        else if (wsstate.changed)
+        {
+            OSC.SendOSCPacket(wsstate.cur ? t : f, oscBuf);
+            oscstate.cur = (wsstate.cur);
+        }
+    }
+    oscstate.changed = false;
+    wsstate.changed = false;
+    Thread.Sleep(100);
+}
+
---
+++ boolchatosc/utils.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.WebSockets;
+
+namespace boolchatosc
+{
+    class statemngr
+    {
+        private bool currentState = false;
+
+        private string name;
+        static private statemngr[] instances = new statemngr[0];
+
+        public statemngr(string name) => this.name = name;
+
+        public bool cur
+        {
+            set
+            {
+                currentState = value;
+                changed = true;
+                Console.WriteLine($"{name} set state to {currentState}");
+            }
+            get { return currentState; }
+        }
+
+        public bool changed = false;
+    }
+
+    class wsmngr
+    {
+        private ClientWebSocket ws = new();
+        private ArraySegment<byte> inBuf = new(new byte[10]);
+        Task<WebSocketReceiveResult> wsRecive;
+        static readonly ArraySegment<byte> t = new(new byte[] { 116, 114, 117, 101 }); //true
+        static readonly ArraySegment<byte> f = new(new byte[] { 102, 097, 108, 115, 101 }); //false
+
+        public wsmngr(Uri uri)
+        {
+            ws.ConnectAsync(uri, new());
+        }
+
+        public void send(bool state)
+        {
+            ws.SendAsync(state ? t : f, WebSocketMessageType.Text, true, new());
+        }
+
+        public bool? receive()
+        {
+            if (wsRecive == null)
+            {
+                if (ws.State == WebSocketState.Open)
+                    wsRecive = ws.ReceiveAsync(inBuf, new());
+                else
+                    return null;
+            }
+
+            if (wsRecive.IsCompleted)
+            {
+                wsRecive = null;
+                return inBuf[0] == 116;//t
+            }
+            return null;
+        }
+    }
+}
